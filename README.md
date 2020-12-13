e858d8dcd7beed8486f70d0ef6520391a04ed220
Adam Moser <63419657+toteki@users.noreply.github.com>
2020-12-13 01:41:47 -0700

    yay

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # webp-to-png
-Converts webp images to png
+
+A utility that, on run, cycles through webp images in its input folder and converts them to png images in its output folder. Deletes any input processed successfully.
+
+# To run
+```
+make
+```
+(As it prompts you, press enter for each input you want to convert - type anything before pressing enter to abort)
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bytes"
+	"github.com/toteki/wiz"
+	"golang.org/x/image/webp"
+	"image/png"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	wiz.MkDir("./input")
+	wiz.MkDir("./output")
+	/////////////////////////////////
+	files, err := ioutil.ReadDir("./input")
+	if err != nil {
+		wiz.Red(err.Error())
+		return
+	}
+	s := []string{}
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".webp") {
+			s = append(s, file.Name())
+		}
+	}
+	wiz.Green(s)
+	/////////////////////////////////
+	i := 0
+	for i < len(s) {
+		if wiz.SilentPrompt(s[i]) == "" {
+			nemu := strings.TrimSuffix(s[i], ".webp")
+			convert(nemu)
+			i++
+		} else {
+			return
+		}
+	}
+}
+
+func convert(f string) {
+	b, err := wiz.ReadFile("./input/" + f + ".webp")
+	if err != nil {
+		wiz.Red(err)
+		return
+	}
+	i, err := webp.Decode(bytes.NewReader(b))
+	if err != nil {
+		wiz.Red(err)
+		return
+	}
+	////////////
+	_, chk := wiz.ReadFile("./output/" + f + ".png")
+	if chk == nil {
+		wiz.Red("Already exists")
+		return
+	}
+	fi, err := os.Create("./output/" + f + ".png")
+	if err != nil {
+		wiz.Red(err)
+		return
+	}
+	defer fi.Close()
+	err = png.Encode(fi, i)
+	if err != nil {
+		wiz.Red(err)
+		return
+	}
+	err = wiz.DeleteFile("./input/" + f + ".webp")
+	if err != nil {
+		wiz.Purple(err)
+		return
+	}
+}
